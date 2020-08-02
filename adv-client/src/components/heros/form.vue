3abd13b27272fcbdf693974f7863905d3b76d39f
hoochicken <mareike.riegel@ql.de>
2020-08-02 17:52:37 +0200

    Heros > Form: data as ONE object

--- adv-client/src/components/heros/form.vue
+++ adv-client/src/components/heros/form.vue
@@ -2,22 +2,22 @@
     <div>
         <form>
             <div class="form-group">
-                <input class="form-control" v-bind="id"/>
-                <input class="form-control" v-bind="name"/>
-                <input class="form-control" v-bind="type"/>
-                <input class="form-control" v-bind="class" />
-                <input class="form-control" v-bind="description"/>
-                <input class="form-control" v-bind="pic"/>
-                <input class="form-control" v-bind="le"/>
-                <input class="form-control" v-bind="le_current"/>
-                <input class="form-control" v-bind="ae"/>
-                <input class="form-control" v-bind="ae_current"/>
-                <input class="form-control" v-bind="inventory"/>
-                <input class="form-control" v-bind="weapon"/>
-                <input class="form-control" v-bind="at"/>
-                <input class="form-control" v-bind="pa"/>
-                <input class="form-control" v-bind="attributes"/>
-                <input class="form-control" v-bind="state"/>
+                <input class="form-control" v-bind="item.id"/>
+                <input class="form-control" v-bind="item.name"/>
+                <input class="form-control" v-bind="item.type"/>
+                <input class="form-control" v-bind="item.class" />
+                <input class="form-control" v-bind="item.description"/>
+                <input class="form-control" v-bind="item.pic"/>
+                <input class="form-control" v-bind="item.le"/>
+                <input class="form-control" v-bind="item.le_current"/>
+                <input class="form-control" v-bind="item.ae"/>
+                <input class="form-control" v-bind="item.ae_current"/>
+                <input class="form-control" v-bind="item.inventory"/>
+                <input class="form-control" v-bind="item.weapon"/>
+                <input class="form-control" v-bind="item.at"/>
+                <input class="form-control" v-bind="item.pa"/>
+                <input class="form-control" v-bind="item.attributes"/>
+                <input class="form-control" v-bind="item.state"/>
             </div>
         </form>
     </div>
@@ -28,20 +28,24 @@
         name: "hero-form",
         data () {
             return {
-                id: 0,
-                name: '',
-                type: 0,
-                class: 0,
-                description: '',
-                pic: '',
-                le: '',
-                le_current: '',
-                inventory: '',
-                weapon: 1,
-                at: 0,
-                pa: 0,
-                attributes: {},
-                state: true
+                item: {
+                    id: 0,
+                    name: '',
+                    type: 0,
+                    class: 0,
+                    description: '',
+                    pic: '',
+                    le: '',
+                    le_current: '',
+                    ae: '',
+                    ae_current: '',
+                    inventory: '',
+                    weapon: 1,
+                    at: 0,
+                    pa: 0,
+                    attributes: {},
+                    state: true
+                }
             }
         }
     }
