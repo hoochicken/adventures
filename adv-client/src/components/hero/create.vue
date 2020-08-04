27ca6d6082dff8370ed2e1ae7b4d5bbba9bf8d2b
hoochicken <mareike.riegel@ql.de>
2020-08-04 11:22:31 +0200

    Hero > Create: Routing to update form implemented bugfix

--- adv-client/src/components/hero/create.vue
+++ adv-client/src/components/hero/create.vue
@@ -37,7 +37,7 @@
             async createHero(item) {
                 var params = JSON.stringify(item);
                 this.response = await this.axios.post('/hero/create', params);
-                this.$router.push('hero/update/' + this.response.data.id)
+                this.$router.push('/hero/update/' + this.response.data.id)
             }
         }
     }
