b7e65797a11648dd474b386f14e795603ecb04cb
hoochicken <mareike.riegel@ql.de>
2020-08-05 10:22:07 +0200

    Global component: search field added with search and reset function

---
+++ adv-client/src/components/global/search.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="input-group">
+        <input class="form-control" v-model="searchterm" v-on:keyup.enter="search" />
+        <div class="input-group-append">
+            <button class="input-group-text" @click="search">Search</button>
+            <button class="input-group-text" @click="resetSearch">Reset</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "search",
+        props: [],
+        data () {
+            return {
+                searchterm: ''
+            }
+        },
+        methods: {
+            search () {
+                this.$emit('search', this.searchterm);
+            },
+            resetSearch () {
+                this.searchterm = '';
+                this.$emit('resetSearch');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
