3c5a2230833b26c79292f08801cdf4c3ef33d886
hoochicken <mareike.riegel@ql.de>
2020-08-27 20:47:31 +0200

    Routes added for places - php and js

--- adv-client/src/main.js
+++ adv-client/src/main.js
@@ -3,9 +3,15 @@ import VueRouter from 'vue-router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Index from './App.vue'
+
 import HeroList from './components/hero/list.vue'
 import HeroCreate from './components/hero/create.vue'
 import HeroUpdate from './components/hero/update.vue'
+
+import PlaceList from './components/place/list.vue'
+import PlaceCreate from './components/place/create.vue'
+import PlaceUpdate from './components/place/update.vue'
+
 import axios from "./plugins/axios";
 import Pagination from 'vue-paginate-al'
 
@@ -18,6 +24,9 @@ const routes = [
   { path: '/hero/list', component: HeroList },
   { path: '/hero/create', component: HeroCreate },
   { path: '/hero/update/:id', component: HeroUpdate },
+  { path: '/place/list', component: PlaceList },
+  { path: '/place/create', component: PlaceCreate },
+  { path: '/place/update/:id', component: PlaceUpdate },
   { path: '/', component: Index }
 ];
 
