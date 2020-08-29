37881e4606da0357a04418487a05c868aa189a03
hoochicken <mareike.riegel@ql.de>
2020-08-29 09:03:59 +0200

    Global components: message box added as functional template

---
+++ adv-client/src/components/global/message-box.vue
@@ -0,0 +1,19 @@
+<template functional>
+    <div :class="'d-flex alert alert-' + msgtype">asd{{msgtype}}asd<slot></slot></div>
+</template>
+
+<script>
+    export default {
+        name: "message-box",
+        props: {
+            msgtype: {
+                type: String,
+                default: 'info'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
