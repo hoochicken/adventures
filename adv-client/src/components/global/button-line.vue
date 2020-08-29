9b4df440be02fa09e64ad9b3b551aa0bbe3350c4
hoochicken <mareike.riegel@ql.de>
2020-08-29 09:03:23 +0200

    Global components: button-line added

---
+++ adv-client/src/components/global/button-line.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :class="'d-flex mb-3 ' + (itemId > 0 ? 'justify-content-end' : 'justify-content-end')">
+        <div v-if="itemId > 0" class="mr-auto btn btn-danger" @click="$emit('delete', itemId)">Delete</div>
+        <button class="btn btn-warning mr-3" @click="$emit('cancel')">Cancel</button>
+        <div v-if="itemId > 0" class="btn btn-success" @click="$emit('update', itemId)">Update</div>
+        <div v-else class="btn btn-success" @click="$emit('create')">Create</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "button-line",
+        props: {
+            itemId: {
+                type: Number,
+                default: 0
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
